b51a0cc961decced12a68a521459aee591e3409e
Hetansh Patel <hetansh.patel@icloud.com>
2021-05-20 00:04:24 -0600

    Initial Commit

---
+++ Shared/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Shared
+//
+//  Created by Hetansh on 5/20/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
