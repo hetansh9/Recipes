84f3d0a4673451b7f6caadf1d8bf9bd17ed76862
Hetansh Patel <hetansh.patel@icloud.com>
2021-05-20 00:13:08 -0600

    bau mast

---
+++ Shared/Model/FoodsModel.swift
@@ -0,0 +1,20 @@
+//
+//  FoodsModel.swift
+//  Recipes
+//
+//  Created by Hetansh on 5/20/21.
+//
+
+import SwiftUI
+
+// MARK: - FOODS DATA MODEL
+
+struct Food: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
