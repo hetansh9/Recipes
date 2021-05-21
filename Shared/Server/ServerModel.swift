846f4719f1957403eb571952c4b5d1883f1e7afd
Hetansh Patel <hetansh.patel@icloud.com>
2021-05-20 20:43:40 -0600

    updated the get request but i am still yet to test it with postman i am going to do that tomorrow

---
+++ Shared/Server/ServerModel.swift
@@ -0,0 +1,26 @@
+//
+//  ServerModel.swift
+//  Recipes (iOS)
+//
+//  Created by Hetansh on 5/20/21.
+//
+
+import Foundation
+
+//MARK: - Todo
+
+//struct Todo: Codable{
+//    let id: Int
+//    let calories: Int
+//    let carbs: String
+//    let fat: String
+//    let image: String
+//    let imageType: String
+//    let protien: String
+//    let tittle: String
+//
+//    enum CodingKeys: String, CodingKeys {
+//        case id = "id"
+//        case calories, t
+//    }
+//}
---
+++ Shared/Server/ServerRequest.swift
@@ -0,0 +1,68 @@
+//
+//  ServerRequest.swift
+//  Recipes (iOS)
+//
+//  Created by Hetansh on 5/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //getting the basic url from rapid apis and testing it out for now
+        let url  = URL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger&diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C%20gluten&number=10&offset=0&type=main%20course")
+        guard url != nil else {
+            print("Unable to retrieve the URL")
+            return
+        }
+        
+        var request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
+        
+        let headers = [
+            "x-rapidapi-key": "8f1a562dfe5446dfa12c9df74680245a",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+            "content-type": "application/json"
+            
+        ]
+        
+        request.allHTTPHeaderFields = headers
+        
+        let jsonObject = [
+            "query": "burger",
+            "diet" : "vegetarian",
+            "excludeingredients": "coconut",
+            "intolerances": "egg,gluten",
+            "number": 10,
+            "offset": 0,
+            "type": "main course"] as [String : Any]
+        
+        do {
+                let requestBody = try JSONSerialization.data(withJSONObject: jsonObject, options: .fragmentsAllowed)
+            request.httpBody = requestBody
+        }
+        catch {
+            print("Error creating object from json")
+        }
+        
+        request.httpMethod = "GET"
+        
+        let session = URLSession.shared
+        
+        let datatask = session.dataTask(with: request) { (data, response, error) in
+                
+            if error == nil && data != nil {
+                
+                do {
+                    let items = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [String:Any]
+                    print(items)
+                }catch {
+                    print("Error")
+                }
+            }
+        }
+        datatask.resume()
+    }
+}
