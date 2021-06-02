f37d6132893abb321643915d5163e20eeaa016c6
Hetansh Patel <hetansh.patel@icloud.com>
2021-06-02 15:37:32 -0600

    redid the server files

---
+++ Shared/Server/RecipeInfo.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeInfo.swift
+//  Recipes (iOS)
+//
+//  Created by Hetansh on 5/30/21.
+//
+
+import Foundation
+
+struct RecipeInfo {
+    
+    var title : String?
+    var imageURL : String?
+    var timeRequired : Int?
+    var sourceURL  : String?
+    var ingredients : [String]?
+    var instructions : [String]?
+    var servings : Int?
+    
+}
--- Shared/Server/ServerRequest.swift
+++ Shared/Server/ServerRequest.swift
@@ -7,63 +7,140 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ServerRequest {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    static let apiKey = "8f1a562dfe5446dfa12c9df74680245a"
+    static let host = "api.spoonacular.com"
+    static let scheme = "https"
+    
+    // configure the recipes such as the tittle and image and everything else
+    
+    private class func configureRecipe(info: [String: Any]) -> RecipeInfo{
+        var recipe = RecipeInfo()
         
-        //getting the basic url from rapid apis and testing it out for now
-        let url  = URL(string: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=burger&diet=vegetarian&excludeIngredients=coconut&intolerances=egg%2C%20gluten&number=10&offset=0&type=main%20course")
-        guard url != nil else {
-            print("Unable to retrieve the URL")
-            return
+        if let title = info["title"] as? String {
+            recipe.title = title
         }
         
-        var request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10)
+        if let servings = info["servings"] as? Int {
+            recipe.servings = servings
+        }
         
+        if let imageURL = info["image"] as? String {
+            recipe.imageURL = imageURL
+        }
+        if let sourceURL = info["sourceURL"] as? String {
+            recipe.sourceURL = sourceURL
+        }
         
-        let headers = [
-            "x-rapidapi-key": "8f1a562dfe5446dfa12c9df74680245a",
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-            "content-type": "application/json"
+        if let ingredientsArray = info["extendedIngredients"] as? [[String: Any]] {
+            if ingredientsArray.count == 0 {
+                recipe.ingredients = []
+            }else {
+                var ingredients = [String]()
+                for i in ingredientsArray{
+                    if let i = i["originalString"] as? String {
+                        ingredients.append(i)
+                    }
+                }
+                recipe.ingredients = ingredients
+            }
+        }else {
+            recipe.ingredients = []
             
-        ]
-        
-        request.allHTTPHeaderFields = headers
-        
-        let jsonObject = [
-            "query": "burger",
-            "diet" : "vegetarian",
-            "excludeingredients": "coconut",
-            "intolerances": "egg,gluten",
-            "number": 10,
-            "offset": 0,
-            "type": "main course"] as [String : Any]
-        
-        do {
-                let requestBody = try JSONSerialization.data(withJSONObject: jsonObject, options: .fragmentsAllowed)
-            request.httpBody = requestBody
-        }
-        catch {
-            print("Error creating object from json")
         }
         
-        request.httpMethod = "GET"
-        
-        let session = URLSession.shared
-        
-        let datatask = session.dataTask(with: request) { (data, response, error) in
-                
-//            if error == nil && data != nil {
-//                
-//                do {
-//                    let items = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? [String:Any]
-//                    
-//                }catch {
-//                    print("Error")
-//                }
-//            }
+        if let time = info["readyInMinutes"] as? Int {
+            recipe.timeRequired = time
         }
-        datatask.resume()
+        
+        if let instructions = info["analyzedInstructions"] as? [[String:Any]] {
+            if instructions.count == 0 {
+                recipe.instructions = []
+            }else {
+                var instructionsArray = [String]()
+                for j in instructions {
+                    if let j = j["Steps"] as? [[String:Any]] {
+                        for k in j {
+                            if let j = k["step"] as? String {
+                                instructionsArray.append(j)
+                            }
+                        }
+                    }
+                }
+                recipe.instructions = instructionsArray
+            }
+            
+        }else {
+            recipe.instructions = []
+        }
+        return recipe
+        
     }
+    
+    
+    class func getSearchRecipe(id: Int, completion: @escaping ( RecipeInfo?, Bool, Error? ) -> Void) {
+        var url : URL{
+            var components = URLComponents()
+            components.host = host
+            components.path = "/recipes/\(id)/information"
+            components.scheme = scheme
+            components.queryItems = [URLQueryItem]()
+            components.queryItems?.append(URLQueryItem(name: "apiKey", value: ServerRequest.apiKey))
+            
+            return components.url!
+
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data,response, error) in
+            if error != nil {
+                completion(nil,false,error)
+                return
+            }
+            guard let data = data else {
+                 completion(nil,false,error)
+                return
+            }
+            do{ //.fragmentsallowed because for json conversatin as the data is not in an array
+                if let responseData = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any] {
+                    let app = configureRecipe(info: responseData)
+                    completion(app, true, nil)
+                }
+            }catch {
+                completion(nil,false,error)
+            }
+        }
+        task.resume()
+    }
+    
+    class func autoComplete(query: String, completion: @escaping ([AutoComplete], Error?) -> Void ) -> URLSessionTask{
+        var result: URL{
+            var components = URLComponents()
+            components.host = host
+            components.path = "/recipes/autocomplete"
+            components.scheme = scheme
+            
+            components.queryItems = [URLQueryItem]()
+            components.queryItems?.append(URLQueryItem(name: "apiKey", value: ServerRequest.apiKey))
+            components.queryItems?.append(URLQueryItem(name: "number", value: "8"))
+            components.queryItems?.append(URLQueryItem(name: "return", value: query))
+            
+            return components.url!
+            
+        }
+        let task = URLSession.shared.dataTask(with: result) { (data,response, error) in
+            guard let data =  data else {
+                completion([], error)
+                return
+            }
+            do {
+                let responseObject = try JSONDecoder().decode([AutoComplete].self, from: data)
+                completion(responseObject, nil)
+            }catch {
+                completion([],error)
+            }
+        }
+        return task
+    }
+     
 }
