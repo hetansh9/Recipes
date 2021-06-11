74afcb3dfd07ba4fd2e07453117c0c668ef05548
Het Prajapati <hprajapati@albany.edu>
2021-06-11 14:23:24 +0530

    Changed FeaturedView and DetailView Changes

--- Shared/App/RecipesDetailedView.swift
+++ Shared/App/RecipesDetailedView.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct RecipesDetailedView: View {
+    //MARK: - PROPERTIES
+    var recipe: RecipesData = recipesData[0]
+    
     var body: some View {
-        Text("RecipesDetailedView")
+        // Container containing the Card
+        
+        VStack(spacing: 16) {
+            // CARD ITSELF
+            VStack(spacing: 0) {
+                Text(recipe.title.uppercased()).font(.largeTitle).bold()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                    .padding()
+                // Lower Banner
+                HStack {
+                    VStack(alignment: .leading, spacing: 4){
+                        Text(recipe.subtitle)
+                            .font(.body).bold()
+                            .offset(x: 5)
+                    }
+                    .foregroundColor(Color.white)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity)
+                .padding(12)
+                .background(
+                    VisualEffectBlur(blurStyle: .systemMaterialDark)
+                )
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: 450)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "background-1"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            )
+            .clipShape(RoundedRectangle(cornerRadius: 0, style: .continuous))
+            .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 10)
+            
+            Spacer()            
+            
+        }//: VStack
+        .ignoresSafeArea()
     }
 }
 
