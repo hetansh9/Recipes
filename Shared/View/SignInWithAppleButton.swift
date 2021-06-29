3a4820e941e6b392711f22325be79a511126083c
Het Prajapati <hprajapati@albany.edu>
2021-06-30 00:08:08 +0530

    button added

---
+++ Shared/View/SignInWithAppleButton.swift
@@ -0,0 +1,20 @@
+//
+//  SignInWithAppleButton.swift
+//  Recipes
+//
+//  Created by Het Prajapati on 6/30/21.
+//
+
+import SwiftUI
+import AuthenticationServices
+import FirebaseAuth
+
+struct SignInWithAppleButton: UIViewRepresentable {
+    typealias UIViewType = ASAuthorizationAppleIDButton
+    func makeUIView(context: Context) -> UIViewType {
+        return ASAuthorizationAppleIDButton(type: .continue, style: .black)
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
